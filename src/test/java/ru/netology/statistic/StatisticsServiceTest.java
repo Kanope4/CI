bccf7a4271a80044316f37bd94e259bd98bda95c
Anastasia N <nastka_kapito@mail.ru>
2022-06-08 20:56:20 +0300

    test 100%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,14 +18,15 @@ public class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
     @Test
-    public void currentMaxEqualIncome() {
+    public void findMaxInTeMiddle() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 12};
+        long expected = 30;
 
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+
 }
